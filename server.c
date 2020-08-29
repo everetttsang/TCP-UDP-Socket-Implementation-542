32decd4816011ebffa339804cb26e96b87854183
everetttsang <everettt@usc.edu>
2020-08-28 22:36:37 -0700

    server loop + sigchld
    server loop integrated and functioning,
    also added ignore sigchld call to reap zombie processes

--- server.c
+++ server.c
@@ -1,7 +1,7 @@
 /* A simple server in the internet domain using TCP
    The port number is passed as an argument */
 #include <stdio.h>
-#include <sys/types.h> 
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <stdlib.h>
@@ -13,10 +13,22 @@ void error(char *msg)
     perror(msg);
     exit(1);
 }
+void dostuff(int sockfd){ //added dummy function to allow child processes to continue handling messages
+  int n;
+  char buffer[256];
+  bzero(buffer,256);
+  n = read(sockfd,buffer,255);
+  if (n < 0) error("ERROR reading from socket");
+  printf("Here is the message: %s\n",buffer);
+  n = write(sockfd,"I got your message",18);
+  if (n < 0) error("ERROR writing to socket");
+}
 
 int main(int argc, char *argv[])
 {
-     int sockfd, newsockfd, portno, clilen;
+     //ignore SIGCHLD signal
+     signal(SIGCHLD,SIG_IGN);
+     int sockfd, newsockfd, portno, clilen, pid;
      char buffer[256];
      struct sockaddr_in serv_addr, cli_addr;
      int n;
@@ -25,7 +37,7 @@ int main(int argc, char *argv[])
          exit(1);
      }
      sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0) 
+     if (sockfd < 0)
         error("ERROR opening socket");
      bzero((char *) &serv_addr, sizeof(serv_addr));
      portno = atoi(argv[1]);
@@ -33,18 +45,26 @@ int main(int argc, char *argv[])
      serv_addr.sin_addr.s_addr = INADDR_ANY;
      serv_addr.sin_port = htons(portno);
      if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0) 
+              sizeof(serv_addr)) < 0)
               error("ERROR on binding");
      listen(sockfd,5);
      clilen = sizeof(cli_addr);
-     newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-     if (newsockfd < 0) 
-          error("ERROR on accept");
-     bzero(buffer,256);
-     n = read(newsockfd,buffer,255);
-     if (n < 0) error("ERROR reading from socket");
-     printf("Here is the message: %s\n",buffer);
-     n = write(newsockfd,"I got your message",18);
-     if (n < 0) error("ERROR writing to socket");
-     return 0; 
+     while(1){
+       newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+       if (newsockfd < 0)
+            error("ERROR on accept");
+       pid = fork();
+       if(pid<0)
+            error("ERROR on fork");
+      if(pid ==0){
+        close(sockfd);
+        dostuff(newsockfd);
+        exit(0);
+      }
+      else
+        close(newsockfd);
+     }
+
+
+     return 0;
 }
